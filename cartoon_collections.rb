69bf7b8ef682fd451637da29817d3b2e7575fb4e
Alexander Frosell <sachafrosell@gmail.com>
2018-10-04 12:35:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,9 @@ end
 summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
 
 def long_planeteer_calls
-  array.include?()
+  array.any? do |word|
+    word.length > 4
+  end
 end
 
 long_planeteer_calls(["puff", "go", "two"])
